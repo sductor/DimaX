7352f277ac410d28252868f05acb0f5ce7e28fbe
ductors <sylvain.ductor@lip6.fr>
2011-12-02 11:24:55 +0100

    chord

--- src/negotiation/experimentationframework/Experimentator.java
+++ src/negotiation/experimentationframework/Experimentator.java
@@ -156,8 +156,8 @@ public class Experimentator extends APIAgent{
 		Experimentator exp = new Experimentator(new ReplicationExperimentationProtocol());
 //										exp.initAPI(false);//SCHEDULED
 //		exp.initAPI(true);//FIPA	
-						exp.initAPI(7779,7778);//DARX LOCAL
-//				exp.initAPI("lip6.xml");//DARX Deployed
+//						exp.initAPI(7779,7778);//DARX LOCAL
+				exp.initAPI("lip6.xml");//DARX Deployed
 		exp.launchMySelf();
 	}
 }

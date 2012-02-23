ff05a2abd98b6bd6d7d50c25d4150cd3add7b19a
vletard <letard.vincent@gmail.com>
2012-02-23 23:17:30 +0100

    package horizon opened + some reverse UML in negotiation

---
+++ src/negotiation/horizon/negociatingagent/SubstrateNodeState.java
@@ -0,0 +1,10 @@
+package negotiation.horizon.negociatingagent;
+
+public class SubstrateNodeState extends HorizonSpecification {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2415474717777657012L;
+
+}

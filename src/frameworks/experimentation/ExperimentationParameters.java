f56d5966088ae8e4e708cf443a16d348c1abb676
ductors <ductors@alister.lip6.fr>
2012-11-30 12:04:02 +0100

    ajust exp

--- src/frameworks/experimentation/ExperimentationParameters.java
+++ src/frameworks/experimentation/ExperimentationParameters.java
@@ -59,7 +59,7 @@ extends BasicAgentModule<Agent> implements Comparable{
 //		public static final long _maxSimulationTime = 60000 * 45;//45 minutes
 //			public static final long _maxSimulationTime = 60000 * 60;//60 minutes
 
-	public static int nbPart=4;
+	public static int nbPart=12;
 
 	//			public  long maxIndividualComputingTime = 60000;//1 min
 	//	public  long maxIndividualComputingTime = 120000;//2 min
